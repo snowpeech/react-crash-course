5be1ab467daa09c0f4d60983bcc4f86a1e6ee7cb
Lydia <snowpeech@gmail.com>
2019-07-02 18:20:16 -0500

    starting nav bar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,50 +2,47 @@ import React, {Component} from 'react';
 var product;
 
 class Counter extends Component {
-    state = {
-        value: this.props.value,      
-    };
-
     // constructor(){
     //     super();
     //     this.handleIncrement = this.handleIncrement.bind(this);
-    // }
-    
+    // } 
     renderTags(){
         if (this.state.tags.length === 0) return <p>no tags</p>;
 
         return <ul> {this.state.tags.map(tag => <li key = {tag}> {tag}</li>)} </ul>
-
     }
-
-    handleIncrement = product => {
-        console.log(product);
-        this.setState({value:this.state.value + 1 }); //"setState" tells react that this component's state is going to change. react will call eventually the render method (unknown timing) and it will return the new DOM
-    }
-    
+    // handleIncrement = product => {
+    //     this.setState({value:this.state.value + 1 }); //"setState" tells react that this component's state is going to change. react will call eventually the render method (unknown timing) and it will return the new DOM
+    // } Deleted because we want the parent, not this controlled element, in charge of the state
     render() {     
         return (
         <div>
             <span className = {this.getBadgeClasses()}> {this.formatCount()}</span>
-            <button onClick={() => this.handleIncrement(product)}
-            className = "btn btn-secondary btn-sm">Increment </button>
+            <button 
+                onClick={() => this.props.onIncrement(this.props.counter)}
+                className = "btn btn-secondary btn-sm"
+            >Increment </button>
             {/* {this.state.tags.length === 0 && 'please make new tag'}
             {this.renderTags() } */}
+            <button 
+            onClick={() => this.props.onDelete(this.props.counter.id)} 
+            className="btn btn-danger btn-sm m-2"
+            >
+                Delete
+            </button>
+
         </div>);
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
+        classes += (this.props.counter.value === 0) ? "warning" : "primary";
         return classes;
     }
-
         formatCount(){
-            const { value: count } = this.state;
-
+            const { value: count } = this.props.counter;
         return count === 0 ? "0" : count
         }
 }
 
- 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -7,13 +7,41 @@ class Counters extends Component {
             {id:1, value: 0},
             {id:2, value: 2},
             {id:3, value: 0},
-            {id:4, value: 0},
+            {id:4, value: 0}
         ]
     }
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({ counters });
+    }
+
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({counters});
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map( c => {
+            c.value = 0;
+            return c
+        })
+        this.setState({counters});
+    }
+
     render() { 
-        return ( <div>
+        return ( 
+        <div>
+            <button 
+                onClick = {this.handleReset}
+                className="btn btn-primary btn-sm m-2">Reset</button>
             { this.state.counters.map(counter => 
-                <Counter key = {counter.id} value = {counter.value}>
+                <Counter key = {counter.id} 
+                onDelete = {this.handleDelete} 
+                onIncrement = {this.handleIncrement} 
+                counter ={counter} >
     
                 </Counter> )}
         </div> );
